8718fbdf33968ab5b58ee5bfa1127ba36104bc2c
5haikh <shaikhfaiz7600@gmail.com>
2020-04-20 15:33:42 +0530

    program to sum of two numbers

---
+++ sumoftwonum.c
@@ -0,0 +1,10 @@
+void main(){
+	int a, b, c;
+	printf("\nprogram to print the sum of two number");
+	printf("\nenter the first number");	
+	scanf("%d", &a);
+	printf("\nenter the second number");
+	scanf("%d", &b);
+	c = a + b;
+	printf("\nthe sum of %d and %d is %d", a, b, c);
+}
